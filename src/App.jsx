f7e7e009722813d2a06d724bb892b63bfe6106f9
ishant-rgb-hub <ishant392007@gmail.com>
2026-04-22 22:17:15 +0530

    navbar complete with folder placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css';
-
+import Navbar from './components/Navbar/navbar';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
-        happy
+        
     </div>
   );
 }
