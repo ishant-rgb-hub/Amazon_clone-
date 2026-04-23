d32f12d2a786f7419ded20fcfed5f9e409061580
ishant-rgb-hub <ishant392007@gmail.com>
2026-04-23 12:10:51 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
 import './App.css';
-import Navbar from './components/Navbar/navbar';
-import Footer from './components/footer/footer';
+import Navbar from './Component/Navbar/navbar';
+import HomeScreen from './Screen/HomeScreen/homeScreen';
+import { Routes,Route } from 'react-router-dom';
+import Products from './Screen/Products/products';
+import Footer from './Component/Footer/footer';
 import Cart from './Screen/Cart/cart';
-
 function App() {
   return (
     <div className="App">
-        
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomeScreen/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/cart' element={<Cart/>} />
+      </Routes>
+      <Footer />
+
+      
     </div>
   );
 }
